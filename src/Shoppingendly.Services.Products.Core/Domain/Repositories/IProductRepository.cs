842469817ee9e8f677929a4bc9ddccea399c2032
pkantorowicz <piotrkantorowicz@gmail.com>
2020-02-12 20:59:04 +0100

    [IMPROVEMENT][DOMAIN][CI] Changed appveyor.yml for only be fired from master or develop and added methods to the generic repository.

--- src/Shoppingendly.Services.Products.Core/Domain/Repositories/IProductRepository.cs
+++ src/Shoppingendly.Services.Products.Core/Domain/Repositories/IProductRepository.cs
@@ -1,6 +1,10 @@
+using Shoppingendly.Services.Products.Core.Domain.Aggregates;
+using Shoppingendly.Services.Products.Core.Domain.Base.SeedWork;
+using Shoppingendly.Services.Products.Core.Domain.ValueObjects;
+
 namespace Shoppingendly.Services.Products.Core.Domain.Repositories
 {
-    public interface IProductRepository
+    public interface IProductRepository : IRepository<Product, ProductId>
     {
     }
 }
