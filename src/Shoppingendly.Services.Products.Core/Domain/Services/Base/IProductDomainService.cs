060d3d107f9c111ebf9d26e3b3d4795ef72fca31
pkantorowicz <piotrkantorowicz@gmail.com>
2020-02-15 15:13:49 +0100

    [IMPROVEMENT][DOMAIN][TESTS] Implemented product domain service and added tests for it.

---
+++ src/Shoppingendly.Services.Products.Core/Domain/Services/Base/IProductDomainService.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Shoppingendly.Services.Products.Core.Domain.Aggregates;
+using Shoppingendly.Services.Products.Core.Domain.Entities;
+using Shoppingendly.Services.Products.Core.Domain.ValueObjects;
+using Shoppingendly.Services.Products.Core.Types;
+
+namespace Shoppingendly.Services.Products.Core.Domain.Services.Base
+{
+    public interface IProductDomainService
+    {
+        Maybe<ProductCategory> GetAssignedCategory(Maybe<Product> product, CategoryId categoryId);
+        Maybe<IEnumerable<ProductCategory>> GetAssignedCategories(Maybe<Product> product);
+        
+        Maybe<Product> AddNewProduct(ProductId productId, CreatorId creatorId, string name,
+            string producer);
+        
+        Maybe<Product> AddNewProduct(ProductId productId, CreatorId creatorId, string name,
+            string producer, IEnumerable<CategoryId> categoryIds);
+
+        bool ChangeProductName(Maybe<Product> product, string name);
+        bool ChangeProductProducer(Maybe<Product> product, string producer);
+        void AssignProductToCategory(Maybe<Product> product, CategoryId categoryId);
+        void DeallocateProductFromCategory(Maybe<Product> product, CategoryId categoryId);
+        void DeallocateProductFromAllCategories(Maybe<Product> product);
+    }
+}
