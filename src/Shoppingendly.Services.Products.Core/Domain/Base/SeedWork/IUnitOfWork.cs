4de392446236c9961de5987945e21d3c8cda5f9f
pkantorowicz <piotrkantorowicz@gmail.com>
2020-02-29 02:53:28 +0100

    [IMPROVEMENT] [INFRASTRUCTURE] Removed implementation of UoW, because dbContext now implemented this interface.

--- src/Shoppingendly.Services.Products.Core/Domain/Base/SeedWork/IUnitOfWork.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Shoppingendly.Services.Products.Core.Domain.Base.SeedWork
-{
-    public interface IUnitOfWork
-    {
-        Task<bool> SaveAsync();
-    }
-}
