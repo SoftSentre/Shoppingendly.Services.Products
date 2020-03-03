9d85a53a6a2b7bb6055467ca55507c650a5687e5
pkantorowicz <piotrkantorowicz@gmail.com>
2020-03-03 11:52:19 +0100

    [FEATURE] [INFRASTRUCTURE] Added settings classes for loggin configurations.

---
+++ src/Shoppingendly.Services.Products.Infrastructure/Logger/Settings/ConsoleSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Shoppingendly.Services.Products.Infrastructure.Logger.Settings
+{
+    public class ConsoleSettings
+    {
+        public string LoggingLevel { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
