01be0c3b62d17aee478ef76bc1609de335d7493b
pkantorowicz <piotrkantorowicz@gmail.com>
2020-03-05 12:25:31 +0100

    [IMPROVEMENT] [LOGGER] Added logger configuration interface.
    Added interface which will be responsible for read values from configuration and configure different types of logger.

---
+++ src/Shoppingendly.Services.Products.Infrastructure/Logger/Configuration/ISerilogConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using Serilog;
+using Shoppingendly.Services.Products.Infrastructure.Options;
+
+namespace Shoppingendly.Services.Products.Infrastructure.Logger.Configuration
+{
+    public interface ISerilogConfiguration
+    {
+        void ConfigureLogger(LoggerConfiguration loggerConfiguration, LoggerSettings loggerSettings,
+            AppOptions appOptions, string environmentName);
+    }
+}
