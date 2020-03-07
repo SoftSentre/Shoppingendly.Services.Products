d1b306e1e9768da01cdad95f5e2dfd07ae1f19b1
pkantorowicz <piotrkantorowicz@gmail.com>
2020-03-07 01:36:43 +0100

    [IMPROVEMENT] [INFRA] [IOC] Added domain autofac module.
    Changes:
    - Added domain module with registration of all domain services eg. productDomainService

---
+++ src/Shoppingendly.Services.Products.Infrastructure/InversionOfControl/Modules/DomainModule.cs
@@ -0,0 +1,24 @@
+using Autofac;
+using Shoppingendly.Services.Products.Core.Domain.Services;
+using Shoppingendly.Services.Products.Core.Domain.Services.Base;
+
+namespace Shoppingendly.Services.Products.Infrastructure.InversionOfControl.Modules
+{
+    public class DomainModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductDomainService>()
+                .As<IProductDomainService>()
+                .InstancePerLifetimeScope();
+            
+            builder.RegisterType<CategoryDomainService>()
+                .As<ICategoryDomainService>()
+                .InstancePerLifetimeScope();
+            
+            builder.RegisterType<CreatorDomainService>()
+                .As<ICreatorDomainService>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
